7af6685e3f4cbe1e99252d660cabda7ea6ddf9e2
Janenjihia <jane.student@moringaschool.com>
2022-06-07 09:43:14 +0300

    Add button to be accessible by all methods

--- app/src/main/java/com/moringaschool/recipo/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipo/MainActivity.java
@@ -3,12 +3,18 @@ package com.moringaschool.recipo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSearchRecipeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mSearchRecipeButton = (Button)findViewById(R.id.searchRecipeButton);
     }
 }
