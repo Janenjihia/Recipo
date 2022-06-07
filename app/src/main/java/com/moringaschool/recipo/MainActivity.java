a74fac3ac5a5c0fd8103e47808a2509b685ae68f
Janenjihia <jane.student@moringaschool.com>
2022-06-07 09:45:16 +0300

    Add clickListener

--- app/src/main/java/com/moringaschool/recipo/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipo/MainActivity.java
@@ -3,7 +3,9 @@ package com.moringaschool.recipo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mSearchRecipeButton;
@@ -16,5 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
 
         mSearchRecipeButton = (Button)findViewById(R.id.searchRecipeButton);
+        mSearchRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Welcome to Recipo", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
