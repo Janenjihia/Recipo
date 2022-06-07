41e2753116da70129426a9851e23ea2c06c0daad
Janenjihia <jane.student@moringaschool.com>
2022-06-07 10:02:36 +0300

    Add function to gather data from edit text

--- app/src/main/java/com/moringaschool/recipo/MainActivity.java
+++ app/src/main/java/com/moringaschool/recipo/MainActivity.java
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mSearchRecipeButton;
+    private EditText mIntroEditText;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        mIntroEditText = (EditText) findViewById(R.id.introEditText);
         mSearchRecipeButton = (Button)findViewById(R.id.searchRecipeButton);
         mSearchRecipeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String intro = mIntroEditText.getText().toString();
+                Log.d(TAG, intro);
                 Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
                 startActivity(intent);
                 Toast.makeText(MainActivity.this, "Welcome to Recipo", Toast.LENGTH_LONG).show();
