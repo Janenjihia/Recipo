00fab83feb3eb6f8e5f6c7dad29b5d6323160aa0
Janenjihia <jane.student@moringaschool.com>
2022-06-08 15:28:38 +0300

    Add recycler viewer for the header image

--- app/src/main/java/com/moringaschool/recipo/ui/DetailActivity.java
+++ app/src/main/java/com/moringaschool/recipo/ui/DetailActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.moringaschool.recipo.R;
+import com.secure.foodycookbook.R;
 
 
 public class DetailActivity extends AppCompatActivity {
