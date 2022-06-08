706bbc6e129f896afec8eeae6d9089d177b9d298
Janenjihia <jane.student@moringaschool.com>
2022-06-08 14:32:53 +0300

    Add category layout

---
+++ app/src/main/java/com/moringaschool/recipo/ui/CategoryActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.recipo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
